19222696a485d1524ca270820b2b68f9ae53c27b
Pratik Rajkotiya <pratik.r@peerbits.com>
2022-01-05 14:42:52 +0530

    [DSC-377] google map now rendered when it has a address.

--- src/app/shared/googlemap/googlemap.component.ts
+++ src/app/shared/googlemap/googlemap.component.ts
@@ -12,7 +12,7 @@ import {} from 'googlemaps';
   templateUrl: './googlemap.component.html',
   styleUrls: ['./googlemap.component.scss'],
 })
-export class GooglemapComponent implements OnInit, AfterViewInit {
+export class GooglemapComponent implements AfterViewInit {
   @Input() coordinates: string;
   @ViewChild('map') mapElement: any;
   map: google.maps.Map;
@@ -20,19 +20,30 @@ export class GooglemapComponent implements OnInit, AfterViewInit {
   longitude: string;
 
 
-
-  ngOnInit(): void {
-    this.setLatitudeAndLongitude();
-  }
-
-  setLatitudeAndLongitude() {
+ async ngAfterViewInit(): Promise<void> {
+   if (this.coordinates.includes('@')) {
     [this.latitude, this.longitude] = this.coordinates
-      .replace('@', '')
-      .split(',');
+    .replace('@', '')
+    .split(',');
+    this.mapInitializer();
+   } else {
+    await this.setLatAndLongFromAddress();
+    this.mapInitializer();
+   }
   }
 
-  ngAfterViewInit(): void {
-    this.mapInitializer();
+  setLatAndLongFromAddress() {
+   return new Promise((reslove,reject) => {
+    new google.maps.Geocoder().geocode({ 'address': this.coordinates },  (results, status) => {
+      if (status === google.maps.GeocoderStatus.OK) {
+       this.latitude = results[0].geometry.location.lat().toString();
+       this.longitude = results[0].geometry.location.lng().toString();
+       reslove(1);
+      } else {
+       reject(1);
+      }
+     });
+    });
   }
 
   mapInitializer() {
