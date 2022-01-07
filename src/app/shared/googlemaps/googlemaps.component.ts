5a1b3221fc1ac55559100c11923c47d73e63063f
Pratik Rajkotiya <pratik.r@peerbits.com>
2022-01-07 17:13:57 +0530

    [DSC-377] retrive google map key from backend.

--- src/app/shared/googlemaps/googlemaps.component.ts
+++ src/app/shared/googlemaps/googlemaps.component.ts
@@ -1,5 +1,7 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, Inject, Input, OnInit, Renderer2, ViewChild, } from '@angular/core';
+import { ConfigurationDataService } from 'src/app/core/data/configuration-data.service';
+import { getFirstCompletedRemoteData } from 'src/app/core/shared/operators';
 
 @Component({
   selector: 'ds-googlemaps',
@@ -15,26 +17,50 @@ export class GooglemapsComponent implements OnInit {
 
   constructor(
     @Inject(DOCUMENT) private _document: Document,
-    private renderer: Renderer2
+    private renderer: Renderer2,
+    private configService: ConfigurationDataService,
   ) {
   }
 
   ngOnInit() {
-    const url = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDnzexA2pwD-7UFicRIctj4-vhkj48G_8A';
-
-    this.loadScript(url).then(() => {
-      this.loadMap();
+    this.configService.findByPropertyName('google.maps.key').pipe(
+      getFirstCompletedRemoteData(),
+    ).subscribe((data) => {
+      this.loadScript(this.buildMapUrl(data?.payload?.values[0])).then(() => {
+        this.loadMap();
+      });
     });
   }
 
-  private loadMap() {
-    const map = new google.maps.Map(this.mapElement.nativeElement, {
-      center: { lat: 0, lng: 0 },
-      zoom: 1,
-    });
+/**
+ *
+ * @param key contains a secret key of a google map
+ * @returns string which has google map url with google map key
+ */
+  buildMapUrl(key: string) {
+    return `https://maps.googleapis.com/maps/api/js?key=${key}`;
   }
 
+  /**
+   * load map map in both the case when matadata has coordinates or address
+   */
+  private async loadMap() {
+    if (this.coordinates.includes('@')) {
+      [this.latitude, this.longitude] = this.coordinates
+        .replace('@', '')
+        .split(',');
+      this.mapInitializer();
+    } else {
+      await this.setLatAndLongFromAddress();
+      this.mapInitializer();
+    }
+  }
 
+/**
+ * 
+ * @param url contains a script url which will be loaded into page
+ * @returns
+ */
   private loadScript(url) {
     return new Promise((resolve, reject) => {
       const script = this.renderer.createElement('script');
@@ -47,22 +73,9 @@ export class GooglemapsComponent implements OnInit {
     });
   }
 
-/*
-  async ngAfterViewInit(): Promise<void> {
-    if (this.coordinates.includes('@')) {
-      [this.latitude, this.longitude] = this.coordinates
-        .replace('@', '')
-        .split(',');
-      this.mapInitializer();
-    } else {
-      await this.setLatAndLongFromAddress();
-      this.mapInitializer();
-    }
-  }
-
-  /!**
+  /**
    * Set latitude and longitude when matadata has a address
-   *!/
+   */
   setLatAndLongFromAddress() {
     return new Promise((reslove, reject) => {
       new google.maps.Geocoder().geocode({ 'address': this.coordinates }, (results, status) => {
@@ -77,9 +90,9 @@ export class GooglemapsComponent implements OnInit {
     });
   }
 
-  /!**
+  /**
    * It initialize a google map to html page
-   *!/
+   */
   mapInitializer() {
     this.map = new google.maps.Map(this.mapElement.nativeElement, {
       center: new google.maps.LatLng(
@@ -99,14 +112,4 @@ export class GooglemapsComponent implements OnInit {
       map: this.map,
     });
   }
-
-  /!**
-   * it's inject a google map script dynamically into page
-   *!/
-  initializeScript(): any {
-    const script = this._document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDnzexA2pwD-7UFicRIctj4-vhkj48G_8A';
-    this._document.body.appendChild(script);
-  }*/
 }
