c2b6e8e812ee42f562ed3e8418432829b43c633e
Pratik Rajkotiya <pratik.r@peerbits.com>
2022-01-19 12:53:58 +0530

    [DSC-377] add test cases in map component.

--- src/app/cris-layout/cris-layout-matrix/cris-layout-box-container/boxes/metadata/rendering-types/map/map.component.spec.ts
+++ src/app/cris-layout/cris-layout-matrix/cris-layout-box-container/boxes/metadata/rendering-types/map/map.component.spec.ts
@@ -1,13 +1,61 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, inject, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { LayoutField } from 'src/app/core/layout/models/box.model';
+import { Item } from 'src/app/core/shared/item.model';
+import { MetadataValue } from 'src/app/core/shared/metadata.models';
+import { TranslateLoaderMock } from 'src/app/shared/mocks/translate-loader.mock';
 import { MapComponent } from './map.component';
 
-describe('MapComponent', () => {
+fdescribe('MapComponent', () => {
   let component: MapComponent;
   let fixture: ComponentFixture<MapComponent>;
 
+  const metadataValue = Object.assign(new MetadataValue(), {
+    'value': '@42.1334,56.7654',
+    'language': null,
+    'authority': null,
+    'confidence': -1,
+    'place': 0
+  });
+
+  const testItem = Object.assign(new Item(),
+    {
+      type: 'item',
+      metadata: {
+        'organization.address.addressLocality': [metadataValue]
+      },
+      uuid: 'test-item-uuid',
+    }
+  );
+
+  const mockField: LayoutField = {
+    'metadata': 'organization.address.addressLocality',
+    'label': 'Preferred name',
+    'rendering': 'MAP',
+    'fieldType': 'METADATA',
+    'style': null,
+    'styleLabel': 'test-style-label',
+    'styleValue': 'test-style-value',
+    'labelAsHeading': false,
+    'valuesInline': true
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useClass: TranslateLoaderMock
+        }
+      }), BrowserAnimationsModule],
+      providers: [
+        { provide: 'fieldProvider', useValue: mockField },
+        { provide: 'itemProvider', useValue: testItem },
+        { provide: 'metadataValueProvider', useValue: metadataValue },
+        { provide: 'renderingSubTypeProvider', useValue: '' },
+      ],
       declarations: [ MapComponent ]
     })
     .compileComponents();
@@ -22,4 +70,10 @@ describe('MapComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should rendered google map.',() => {
+    const container = fixture.debugElement.query(By.css('#googlemap'));
+    expect(container).toBeTruthy();
+  });
+
 });
