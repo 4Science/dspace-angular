4ecc482f0eb4eb8cfe1d4e9b839cdf2f594b5f2a
Vincenzo Mecca <vincenzo.mecca@4science.com>
2022-11-17 18:07:46 +0100

    [DSC-629] Removed subscription in pdf-bitstream-viewer for tap and ngVar

--- src/app/item-page/viewer-provider/viewers/bitstream-viewers/pdf-bitstream-viewer/pdf-bitstream-viewer.component.ts
+++ src/app/item-page/viewer-provider/viewers/bitstream-viewers/pdf-bitstream-viewer/pdf-bitstream-viewer.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { BaseBitstreamViewerComponent } from '../base-bitstream-viewer.component';
-import { filter, map, switchMap } from 'rxjs/operators';
+import { filter, map, switchMap, tap } from 'rxjs/operators';
 import { Observable, Subscription } from 'rxjs';
 import { FileService } from '../../../../../core/shared/file.service';
 
@@ -9,35 +9,27 @@ import { FileService } from '../../../../../core/shared/file.service';
   templateUrl: './pdf-bitstream-viewer.component.html',
   styleUrls: ['./pdf-bitstream-viewer.component.scss']
 })
-export class PdfBitstreamViewerComponent extends BaseBitstreamViewerComponent implements OnInit, OnDestroy {
+export class PdfBitstreamViewerComponent extends BaseBitstreamViewerComponent implements OnInit {
 
   @ViewChild('pdfViewer') pdfViewer;
-
+  pdfSrc$: Observable<Blob>;
   private subscription: Subscription;
 
   constructor(private readonly fileService: FileService) {
     super();
   }
 
-  pdfSrc$: Observable<Blob>;
-
   ngOnInit(): void {
     this.pdfSrc$ = this.bitstream$.pipe(
       map(bitstream => bitstream?._links?.content?.href),
       filter(Object),
-      switchMap(href => this.fileService.downloadFile(href))
+      switchMap(href => this.fileService.downloadFile(href)),
+      filter(Object),
+      tap(_ => this.refreshViewer())
     );
-    this.subscription =
-      this.pdfSrc$
-        .subscribe(blob => this.refreshViewer(blob));
   }
 
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
-
-  refreshViewer(blob: Blob) {
-    this.pdfViewer.pdfSrc = blob;
+  refreshViewer() {
     this.pdfViewer.refresh();
   }
 
--- src/app/item-page/viewer-provider/viewers/viewers-shared.module.ts
+++ src/app/item-page/viewer-provider/viewers/viewers-shared.module.ts
@@ -4,6 +4,7 @@ import { IIIFItemViewerComponent } from './item-viewers/iiif-item-viewer/iiif-it
 import { PdfBitstreamViewerComponent } from './bitstream-viewers/pdf-bitstream-viewer/pdf-bitstream-viewer.component';
 import { PdfJsViewerModule } from 'ng2-pdfjs-viewer';
 import { MiradorViewerModule } from '../../mirador-viewer/mirador-viewer.module';
+import { SharedModule } from '../../../shared/shared.module';
 
 const COMPONENTS = [
   IIIFItemViewerComponent,
@@ -17,7 +18,8 @@ const COMPONENTS = [
   imports: [
     CommonModule,
     PdfJsViewerModule,
-    MiradorViewerModule
+    MiradorViewerModule,
+    SharedModule
   ],
   exports: [
     ...COMPONENTS
