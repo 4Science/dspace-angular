cac48db95cec374e458c29bdb240d2859aebc0ec
Vincenzo Mecca <vincenzo.mecca@4science.com>
2022-11-28 17:00:40 +0100

    [IIIF-104] New canvasId for mirador viewer

--- src/app/item-page/iiif-toolbar/iiif-toolbar.component.ts
+++ src/app/item-page/iiif-toolbar/iiif-toolbar.component.ts
@@ -5,6 +5,7 @@ import { Item } from '../../core/shared/item.model';
 import { NotificationsService } from '../../shared/notifications/notifications.service';
 import { TranslateService } from '@ngx-translate/core';
 import { getItemViewerPath } from '../item-page-routing-paths';
+import { Bitstream } from '../../core/shared/bitstream.model';
 
 @Component({
   selector: 'ds-iiif-toolbar',
@@ -13,11 +14,21 @@ import { getItemViewerPath } from '../item-page-routing-paths';
 })
 export class IIIFToolbarComponent implements OnInit {
 
+  private readonly MD_CANVASINDEX = 'bitstream.iiif.canvasindex';
+  private readonly MD_BITSTREAMS_MAP = [{
+    param: 'canvasId',
+    metadata: this.MD_CANVASINDEX
+  }];
+
   @Input()
   item: Item;
 
+  @Input()
+  bitstream: Bitstream;
+
   // The path to the REST manifest endpoint.
   manifestUrl: string;
+  queryParams: {[key: string]: string};
 
   constructor(
     protected router: Router,
@@ -29,10 +40,21 @@ export class IIIFToolbarComponent implements OnInit {
 
   ngOnInit(): void {
     this.manifestUrl = environment.rest.baseUrl + '/iiif/' + this.item.id + '/manifest';
+    this.queryParams = this.getQueryParams();
+  }
+
+  private getQueryParams() {
+    return this.MD_BITSTREAMS_MAP
+      .filter(param => this.bitstream?.metadata[`${param}`].length > 0)
+      .map(param => ({ [`${param.param}`]: this.bitstream?.metadata[`${param.metadata}`][0]?.value }))
+      .reduce((acc, curr) => ({ ...acc, ...curr }), {});
   }
 
   openMiradorViewer() {
-    this.router.navigate([getItemViewerPath(this.item, 'iiif')]);
+    this.router.navigate(
+      [getItemViewerPath(this.item, 'iiif')],
+      { queryParams: this.queryParams }
+    );
   }
 
   iiif() {
