f24d6b16bb3c8f0c1b015e3a3daa0c79b2a45f05
Sufiyan Shaikh <sufiyan.shaikh@4science.com>
2023-01-19 13:44:05 +0530

    [GLAM-241] PR fixes

--- src/themes/glam/app/browse-most-elements/browse-most-elements.component.ts
+++ src/themes/glam/app/browse-most-elements/browse-most-elements.component.ts
@@ -1,62 +1,20 @@
-import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
-import { SearchService } from '../../../../app/core/shared/search/search.service';
-import { DSpaceObject } from '../../../../app/core/shared/dspace-object.model';
-import { Context } from '../../../../app/core/shared/context.model';
-import { RemoteData } from '../../../../app/core/data/remote-data';
-import { PaginatedList } from '../../../../app/core/data/paginated-list.model';
-import { getFirstCompletedRemoteData } from '../../../../app/core/shared/operators';
-import { TopSection, LayoutModeEnum } from '../../../../app/core/layout/models/section.model';
-import { CollectionElementLinkType } from '../../../../app/shared/object-collection/collection-element-link.type';
-import { PaginatedSearchOptions } from '../../../../app/shared/search/models/paginated-search-options.model';
-import { SearchResult } from '../../../../app/shared/search/models/search-result.model';
-import { followLink } from '../../../../app/shared/utils/follow-link-config.model';
+import { Component } from '@angular/core';
 import { getItemPageRoute } from '../../../../app/item-page/item-page-routing-paths';
+import { BrowseMostElementsComponent as BaseComponent } from '../../../../app/shared/browse-most-elements/browse-most-elements.component';
+
 
 @Component({
   selector: 'ds-browse-most-elements',
   styleUrls: ['./browse-most-elements.component.scss'],
   templateUrl: './browse-most-elements.component.html',
 })
-export class BrowseMostElementsComponent implements OnInit {
-  @Input() paginatedSearchOptions: PaginatedSearchOptions;
-
-  @Input() context: Context;
-
-  @Input() topSection: TopSection;
-
-  @Input() mode: LayoutModeEnum;
-
-  searchResults: RemoteData<PaginatedList<SearchResult<DSpaceObject>>>;
-
-  public cardLayoutMode = LayoutModeEnum.CARD;
-
-  public collectionElementLinkTypeEnum = CollectionElementLinkType;
-
-  constructor(
-    private searchService: SearchService,
-    private cdr: ChangeDetectorRef,
-  ) {
-    /* */
-  }
-
-  ngOnInit() {
-    this.getSearchResults();
-  }
-
-  private getSearchResults() {
-    this.searchService
-      .search(this.paginatedSearchOptions, null, true, true, followLink('thumbnail'))
-      .pipe(getFirstCompletedRemoteData())
-      .subscribe(
-        (response: RemoteData<PaginatedList<SearchResult<DSpaceObject>>>) => {
-          this.searchResults = response as any;
-          this.cdr.detectChanges();
-        }
-      );
-  }
-
+export class BrowseMostElementsComponent extends BaseComponent {
+/**
+ * to get the route of the item
+ * @param item
+ * @returns route to the item as a string
+ */
   getItemPageRoute(item) {
     return getItemPageRoute(item);
   }
-
 }
--- src/themes/glam/app/shared/explore/section-component/top-section/top-section.component.ts
+++ src/themes/glam/app/shared/explore/section-component/top-section/top-section.component.ts
@@ -1,10 +1,5 @@
-import { LayoutModeEnum, TopSection } from '../../../../../../../app/core/layout/models/section.model';
-import { Component, Input, OnInit } from '@angular/core';
-
-import { SortDirection, SortOptions } from '../../../../../../../app/core/cache/models/sort-options.model';
-import { Context } from '../../../../../../../app/core/shared/context.model';
-import { PaginatedSearchOptions } from '../../../../../../../app/shared/search/models/paginated-search-options.model';
-import { PaginationComponentOptions } from '../../../../../../../app/shared/pagination/pagination-component-options.model';
+import { Component } from '@angular/core';
+import { TopSectionComponent as BaseComponent} from '../../../../../../../app/shared/explore/section-component/top-section/top-section.component';
 
 /**
  * Component representing the Top component section.
@@ -13,35 +8,5 @@ import { PaginationComponentOptions } from '../../../../../../../app/shared/pagi
   selector: 'ds-top-section',
   templateUrl: './top-section.component.html'
 })
-export class TopSectionComponent implements OnInit {
-
-  @Input()
-  sectionId: string;
-
-  @Input()
-  topSection: TopSection;
-
-  @Input()
-  context: Context = Context.BrowseMostElements;
-
-  paginatedSearchOptions: PaginatedSearchOptions;
-
-  layoutMode: LayoutModeEnum = LayoutModeEnum.CARD;
-
-  ngOnInit() {
-    const order = this.topSection.order;
-    const numberOfItems = this.topSection.numberOfItems;
-    const sortDirection = order && order.toUpperCase() === 'ASC' ? SortDirection.ASC : SortDirection.DESC;
-    const pagination = Object.assign(new PaginationComponentOptions(), {
-      id: 'search-object-pagination',
-      pageSize: numberOfItems,
-      currentPage: 1
-    });
-    this.layoutMode = this.topSection.defaultLayoutMode;
-    this.paginatedSearchOptions = new PaginatedSearchOptions({
-      configuration: this.topSection.discoveryConfigurationName,
-      pagination: pagination,
-      sort: new SortOptions(this.topSection.sortField, sortDirection)
-    });
-  }
+export class TopSectionComponent extends BaseComponent {
 }
