5ca08f97fbb82427c52b951d8cb32f51314f608a
Davide Negretti <davide.negretti@4science.com>
2023-09-18 10:04:15 +0200

    [GLAM-434] process form

--- src/app/admin/admin-mag-import/mag-import-page.component.spec.ts
+++ src/app/admin/admin-mag-import/mag-import-page.component.spec.ts
@@ -2,15 +2,49 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MagImportPageComponent } from './mag-import-page.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { NotificationsService } from '../../shared/notifications/notifications.service';
+import { ScriptDataService } from '../../core/data/processes/script-data.service';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { NotificationsServiceStub } from '../../shared/testing/notifications-service.stub';
+import { createSuccessfulRemoteDataObject$ } from '../../shared/remote-data.utils';
+import { FormsModule } from '@angular/forms';
 
 describe('AdminMagImportComponent', () => {
   let component: MagImportPageComponent;
   let fixture: ComponentFixture<MagImportPageComponent>;
 
+  let notificationService: NotificationsServiceStub;
+  let scriptService: any;
+  let router;
+  let locationStub;
+
   beforeEach(async () => {
+    notificationService = new NotificationsServiceStub();
+    scriptService = jasmine.createSpyObj('scriptService',
+      {
+        invoke: createSuccessfulRemoteDataObject$({ processId: '46' })
+      }
+    );
+    router = jasmine.createSpyObj('router', {
+      navigateByUrl: jasmine.createSpy('navigateByUrl')
+    });
+    locationStub = jasmine.createSpyObj('location', {
+      back: jasmine.createSpy('back')
+    });
+
     await TestBed.configureTestingModule({
       declarations: [ MagImportPageComponent ],
-      imports: [ TranslateModule.forRoot() ]
+      imports: [
+        FormsModule,
+        TranslateModule.forRoot(),
+      ],
+      providers: [
+        { provide: NotificationsService, useValue: notificationService },
+        { provide: ScriptDataService, useValue: scriptService },
+        { provide: Router, useValue: router },
+        { provide: Location, useValue: locationStub },
+      ],
     })
     .compileComponents();
   });
--- src/app/admin/admin-mag-import/mag-import-page.component.ts
+++ src/app/admin/admin-mag-import/mag-import-page.component.ts
@@ -1,15 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {
+  PACKAGER_SCRIPT_NAME,
+  ScriptDataService,
+} from '../../core/data/processes/script-data.service';
+import { ProcessParameter } from '../../process-page/processes/process-parameter.model';
+import { getFirstCompletedRemoteData } from '../../core/shared/operators';
+import { RemoteData } from '../../core/data/remote-data';
+import { Process } from '../../process-page/processes/process.model';
+import { isNotEmpty } from '../../shared/empty.util';
+import { getProcessDetailRoute } from '../../process-page/process-page-routing.paths';
+import { TranslateService } from '@ngx-translate/core';
+import { NotificationsService } from '../../shared/notifications/notifications.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ds-admin-mag-import',
   templateUrl: './mag-import-page.component.html',
   styleUrls: ['./mag-import-page.component.scss']
 })
-export class MagImportPageComponent implements OnInit {
+export class MagImportPageComponent {
 
-  constructor() { }
+  typeParam: string;
+  objectParam: string;
+  fileObject: File;
 
-  ngOnInit(): void {
+  optionList = [ 'MAG', 'METS' ];
+
+  constructor(
+    protected scriptDataService: ScriptDataService,
+    protected translate: TranslateService,
+    protected notificationsService: NotificationsService,
+    private router: Router,
+  ) { }
+
+  setFile(file) {
+    this.fileObject = file;
+  }
+
+  onSubmit() {
+    const parameterValues = new Array<ProcessParameter>();
+
+    parameterValues.push(Object.assign(new ProcessParameter(), { name: '--type', value: this.typeParam }));
+    parameterValues.push(Object.assign(new ProcessParameter(), { name: '--parent', value: this.objectParam }));
+    parameterValues.push(Object.assign(new ProcessParameter(), { name: '--file-path', value: this.fileObject.name }));
+
+    this.scriptDataService.invoke(PACKAGER_SCRIPT_NAME, parameterValues, [this.fileObject]).pipe(
+      getFirstCompletedRemoteData(),
+    ).subscribe((rd: RemoteData<Process>) => {
+      if (rd.hasSucceeded) {
+        const title = this.translate.get('process.new.notification.success.title');
+        const content = this.translate.get('process.new.notification.success.content');
+        this.notificationsService.success(title, content);
+        if (isNotEmpty(rd.payload)) {
+          this.router.navigateByUrl(getProcessDetailRoute(rd.payload.processId));
+        }
+      } else {
+        const title = this.translate.get('process.new.notification.error.title');
+        const content = this.translate.get('process.new.notification.error.content');
+        this.notificationsService.error(title, content);
+      }
+    });
   }
 
 }
