cd4307b885539e4b15f48324c7d67381908efb41
Davide Negretti <davide.negretti@4science.com>
2023-11-07 16:34:55 +0100

    [GLAM-477] Move height calculation outside the template

--- src/themes/glam/app/carousel/carousel.component.ts
+++ src/themes/glam/app/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CarouselComponent as BaseComponent} from '../../../../app/shared/carousel/carousel.component';
 
 /**
@@ -10,5 +10,14 @@ import { CarouselComponent as BaseComponent} from '../../../../app/shared/carous
   styleUrls: ['./carousel.component.scss'],
   providers: []
 })
-export class CarouselComponent extends BaseComponent {
+export class CarouselComponent extends BaseComponent implements OnInit {
+
+  carouselHeight: string;
+
+  ngOnInit() {
+    super.ngOnInit();
+    this.carouselHeight = this.carouselOptions.keepAspectRatio ?
+      `calc(100vw / ${ this.carouselOptions.aspectRatio})` : `${this.carouselOptions.carouselHeightPx}px`;
+  }
+
 }
