3865b2ae92350d2891d96ce70b010123165dc9ad
Francesco Pio Scognamiglio <francescopio.scognamiglio@4science.it>
2023-11-08 18:34:04 +0100

    [GLAM-356] fix mag import component: selector for collections; handle remote url

--- src/app/admin/admin-mag-import/mag-import-page.component.ts
+++ src/app/admin/admin-mag-import/mag-import-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 import {
   PACKAGER_SCRIPT_NAME,
   ScriptDataService,
@@ -7,11 +8,18 @@ import { ProcessParameter } from '../../process-page/processes/process-parameter
 import { getFirstCompletedRemoteData } from '../../core/shared/operators';
 import { RemoteData } from '../../core/data/remote-data';
 import { Process } from '../../process-page/processes/process.model';
-import { isNotEmpty } from '../../shared/empty.util';
+import { isEmpty, isNotEmpty } from '../../shared/empty.util';
 import { getProcessDetailRoute } from '../../process-page/process-page-routing.paths';
 import { TranslateService } from '@ngx-translate/core';
 import { NotificationsService } from '../../shared/notifications/notifications.service';
 import { Router } from '@angular/router';
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { DSONameService } from "../../core/breadcrumbs/dso-name.service";
+import {
+  ImportBatchSelectorComponent
+} from "../../shared/dso-selector/modal-wrappers/import-batch-selector/import-batch-selector.component";
+import { take } from "rxjs/operators";
+import { DSpaceObject } from "../../core/shared/dspace-object.model";
 
 @Component({
   selector: 'ds-admin-mag-import',
@@ -19,32 +27,79 @@ import { Router } from '@angular/router';
   styleUrls: ['./mag-import-page.component.scss']
 })
 export class MagImportPageComponent {
-
-  typeParam: string;
-  objectParam: string;
+  /**
+   * The current value of the file
+   */
   fileObject: File;
 
-  // Import type is currently hardcoded
-  optionList = [ 'MAG', 'METS' ];
+  /**
+   * dso object for community or collection
+   */
+  dso: DSpaceObject = null;
 
-  constructor(
-    protected scriptDataService: ScriptDataService,
-    protected translate: TranslateService,
-    protected notificationsService: NotificationsService,
-    private router: Router,
-  ) { }
+  /**
+   * The flag between upload and url
+   */
+  isUpload = true;
 
+  /**
+   * File URL when flag is for url
+   */
+  fileURL: string;
+
+  public constructor(private location: Location,
+                     protected translate: TranslateService,
+                     protected notificationsService: NotificationsService,
+                     private scriptDataService: ScriptDataService,
+                     private router: Router,
+                     private modalService: NgbModal,
+                     private dsoNameService: DSONameService) {
+  }
+
+  /**
+   * Set file
+   * @param file
+   */
   setFile(file) {
     this.fileObject = file;
   }
 
-  onSubmit() {
-    const parameterValues = new Array<ProcessParameter>();
+  /**
+   * When return button is pressed go to previous location
+   */
+  public onReturn() {
+    this.location.back();
+  }
 
-    parameterValues.push(Object.assign(new ProcessParameter(), { name: '--type', value: 'MAG' })); // hard coded
-    // parameterValues.push(Object.assign(new ProcessParameter(), { name: '--type', value: this.typeParam }));
-    parameterValues.push(Object.assign(new ProcessParameter(), { name: '--parent', value: this.objectParam }));
-    parameterValues.push(Object.assign(new ProcessParameter(), { name: '--file-path', value: this.fileObject.name }));
+  public selectCollection() {
+    const modalRef = this.modalService.open(ImportBatchSelectorComponent);
+    modalRef.componentInstance.response.pipe(take(1)).subscribe((dso) => {
+      this.dso = dso || null;
+    });
+  }
+
+  /**
+   * Starts import-metadata script with --zip fileName (and the selected file)
+   */
+  public import() {
+    if (this.fileObject == null && isEmpty(this.fileURL)) {
+      if (this.isUpload) {
+        this.notificationsService.error(this.translate.get('admin.metadata-import.page.error.addFile'));
+      } else {
+        this.notificationsService.error(this.translate.get('admin.metadata-import.page.error.addFileUrl'));
+      }
+    } else {
+      const parameterValues = new Array<ProcessParameter>();
+
+      parameterValues.push(Object.assign(new ProcessParameter(), {name: '--type', value: 'MAG'})); // hard coded
+      parameterValues.push(Object.assign(new ProcessParameter(), {name: '--parent', value: this.dso.uuid}));
+
+      if (this.isUpload) {
+        parameterValues.push(Object.assign(new ProcessParameter(), { name: '--zip', value: this.fileObject.name }));
+      } else {
+        this.fileObject = null;
+        parameterValues.push(Object.assign(new ProcessParameter(), { name: '--url', value: this.fileURL }));
+      }
 
     this.scriptDataService.invoke(PACKAGER_SCRIPT_NAME, parameterValues, [this.fileObject]).pipe(
       getFirstCompletedRemoteData(),
@@ -63,5 +118,29 @@ export class MagImportPageComponent {
       }
     });
   }
+  }
 
+  /**
+   * return selected dspace object name
+  */
+  getDspaceObjectName(): string {
+    if (this.dso) {
+      return this.dsoNameService.getName(this.dso);
+    }
+    return null;
+  }
+
+  /**
+   * remove selected dso object
+   */
+  removeDspaceObject(): void {
+    this.dso = null;
+  }
+
+  /**
+   * toggle the flag between upload and url
+   */
+  toggleUpload() {
+    this.isUpload = !this.isUpload;
+  }
 }
