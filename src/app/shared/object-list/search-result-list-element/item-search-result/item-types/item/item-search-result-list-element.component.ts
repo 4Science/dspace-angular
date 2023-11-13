3dfd8d285b29b4188862fe0368e3d430c60abc66
frabacche <francesco.bacchelli@4science.com>
2023-11-13 11:58:21 +0100

    GLAM-529 show fulltext(s) in list search item - ts fix

--- src/app/shared/object-list/search-result-list-element/item-search-result/item-types/item/item-search-result-list-element.component.ts
+++ src/app/shared/object-list/search-result-list-element/item-search-result/item-types/item/item-search-result-list-element.component.ts
@@ -42,9 +42,9 @@ export class ItemSearchResultListElementComponent extends SearchResultListElemen
 
   itemViewerRoute: string;
 
-  fullTextMirador:string[];
+  fullTextMirador: string[];
 
-  fullTextVideo:string[];
+  fullTextVideo: string[];
 
   ngOnInit(): void {
     super.ngOnInit();
@@ -54,8 +54,6 @@ export class ItemSearchResultListElementComponent extends SearchResultListElemen
     this.fullTextHighlights = this.allMetadataValues('fulltext');
     this.fullTextMirador = this.allMetadataValues('fulltext.mirador');
     this.fullTextVideo = this.allMetadataValues('fulltext.video');
-    console.log('mirador= ',this.fullTextMirador);
-    console.log('video = ', this.fullTextVideo);
   }
 
 }
