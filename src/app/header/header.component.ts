db80969cd2b16fe0c4ae6c24e89a511d929c25d6
Vlad Nouski <uladzislau.nouski@4science.com>
2023-12-26 15:47:04 +0100

    [GLAM-602] feature: improve header responsiveness

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { MenuService } from '../shared/menu/menu.service';
 import { MenuID } from '../shared/menu/menu-id.model';
+import { HostWindowService } from '../shared/host-window.service';
+import { environment } from 'src/environments/environment';
 
 /**
  * Represents the header with the logo and simple navigation
@@ -11,18 +13,25 @@ import { MenuID } from '../shared/menu/menu-id.model';
   styleUrls: ['header.component.scss'],
   templateUrl: 'header.component.html',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   /**
    * Whether user is authenticated.
    * @type {Observable<string>}
    */
   public isAuthenticated: Observable<boolean>;
+  public isXsOrSm$: Observable<boolean>;
   public showAuth = false;
   menuID = MenuID.PUBLIC;
+  mainSiteUrl = environment.mainSiteUrl;
 
   constructor(
-    private menuService: MenuService
+    private menuService: MenuService,
+    protected windowService: HostWindowService,
   ) {
+  }
+
+  ngOnInit(): void {
+    this.isXsOrSm$ = this.windowService.isXsOrSm();
   }
 
   public toggleNavbar(): void {
