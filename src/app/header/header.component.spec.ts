5c8dbfa3afdd418e8b883e57e66ebe2e8a8fc2a1
Vlad Nouski <uladzislau.nouski@4science.com>
2023-12-27 08:54:29 +0100

    [GLAM-602] fix: unit-test

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -10,6 +10,8 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { MenuService } from '../shared/menu/menu.service';
 import { MenuServiceStub } from '../shared/testing/menu-service.stub';
+import { HostWindowService } from '../shared/host-window.service';
+import { HostWindowServiceStub } from '../shared/testing/host-window-service.stub';
 
 let comp: HeaderComponent;
 let fixture: ComponentFixture<HeaderComponent>;
@@ -26,7 +28,8 @@ describe('HeaderComponent', () => {
         ReactiveFormsModule],
       declarations: [HeaderComponent],
       providers: [
-        { provide: MenuService, useValue: menuService }
+        { provide: MenuService, useValue: menuService },
+        { provide: HostWindowService, useValue: new HostWindowServiceStub(0) },
       ],
       schemas: [NO_ERRORS_SCHEMA]
     })
