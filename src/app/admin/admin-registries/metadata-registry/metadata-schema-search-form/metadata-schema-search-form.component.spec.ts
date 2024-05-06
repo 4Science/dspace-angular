364459c9b742d7c5660a90512aabeee3007a5412
Andrea Barbasso <andrea.barbasso@4science.com>
2024-05-06 11:22:07 +0200

    [GLAM-669] fix tests

--- src/app/admin/admin-registries/metadata-registry/metadata-schema-search-form/metadata-schema-search-form.component.spec.ts
+++ src/app/admin/admin-registries/metadata-registry/metadata-schema-search-form/metadata-schema-search-form.component.spec.ts
@@ -1,17 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MetadataSchemaSearchFormComponent } from './metadata-schema-search-form.component';
+import { FormBuilderService } from '../../../../shared/form/builder/form-builder.service';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { of as observableOf } from 'rxjs';
+import { EventEmitter } from '@angular/core';
 
 describe('MetadataSchemaSearchFormComponent', () => {
   let component: MetadataSchemaSearchFormComponent;
   let fixture: ComponentFixture<MetadataSchemaSearchFormComponent>;
 
+  const translateServiceStub = {
+    get: () => observableOf('test-message'),
+    onLangChange: new EventEmitter(),
+    onTranslationChange: new EventEmitter(),
+    onDefaultLangChange: new EventEmitter()
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MetadataSchemaSearchFormComponent ]
+      declarations: [MetadataSchemaSearchFormComponent],
+      imports: [
+        TranslateModule.forRoot()
+      ],
+      providers: [
+        {provide: FormBuilderService, useValue: {}},
+        {provide: TranslateService, useValue: translateServiceStub},
+      ]
     })
-    .compileComponents();
-  });
+      .compileComponents();
+  })
+  ;
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MetadataSchemaSearchFormComponent);
