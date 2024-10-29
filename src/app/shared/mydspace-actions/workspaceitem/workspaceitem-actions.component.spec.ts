1ac8ad70193d89324e5366e69b9c76d1d605d529
FrancescoMolinaro <francesco.molinaro@4science.com>
2024-10-29 12:55:58 +0100

    [DSC-1956] fix tests to read auth from workspace item

--- src/app/shared/mydspace-actions/workspaceitem/workspaceitem-actions.component.spec.ts
+++ src/app/shared/mydspace-actions/workspaceitem/workspaceitem-actions.component.spec.ts
@@ -71,10 +71,19 @@ const item = Object.assign(new Item(), {
         value: '2015-06-26'
       }
     ]
-  }
+  },
 });
 const rd = createSuccessfulRemoteDataObject(item);
-mockObject = Object.assign(new WorkspaceItem(), { item: observableOf(rd), id: '1234', uuid: '1234' });
+mockObject = Object.assign(new WorkspaceItem(), {
+  item: observableOf(rd),
+  id: '1234',
+  uuid: '1234',
+  _links: {
+    self: {
+      href: 'selfHref'
+    }
+  }
+});
 
 const ePersonMock: EPerson = Object.assign(new EPerson(), {
   handle: null,
