2a3ce26070f419bc96d50c38f5c666690cdd7147
FrancescoMolinaro <francesco.molinaro@4science.com>
2025-06-04 12:09:27 +0200

    [UXP-88] adapt query param handling

--- src/app/cris-layout/cris-layout-matrix/cris-layout-box-container/boxes/metadata/rendering-types/advanced-attachment/bitstream-attachment/attachment-render/types/iiif-toolbar/iiif-toolbar.component.spec.ts
+++ src/app/cris-layout/cris-layout-matrix/cris-layout-box-container/boxes/metadata/rendering-types/advanced-attachment/bitstream-attachment/attachment-render/types/iiif-toolbar/iiif-toolbar.component.spec.ts
@@ -20,11 +20,16 @@ import { createSuccessfulRemoteDataObject$ } from '../../../../../../../../../..
 import { AuthorizationDataServiceStub } from '../../../../../../../../../../../shared/testing/authorization-service.stub';
 import { NotificationsServiceStub } from '../../../../../../../../../../../shared/testing/notifications-service.stub';
 import { IIIFToolbarComponent } from './iiif-toolbar.component';
+import { MockBitstream1 } from '../../../../../../../../../../../shared/mocks/item.mock';
+import { Bitstream } from '../../../../../../../../../../../core/shared/bitstream.model';
+import { MetadataValue } from '../../../../../../../../../../../core/shared/metadata.models';
 
 describe('IiifToolbarComponent', () => {
   let component: IIIFToolbarComponent;
   let fixture: ComponentFixture<IIIFToolbarComponent>;
   let translateService: TranslateService;
+
+  const mockBitstream: Bitstream = MockBitstream1;
 
   const mockItem: Item = Object.assign(new Item(), {
     bundles: createSuccessfulRemoteDataObject$(buildPaginatedList(new PageInfo(), [])),
@@ -73,6 +78,7 @@ describe('IiifToolbarComponent', () => {
     fixture = TestBed.createComponent(IIIFToolbarComponent);
     component = fixture.componentInstance;
     component.item = mockItem;
+    component.bitstream = mockBitstream;
     component.getObjectUrl = () => 'https://test-url.com';
     fixture.detectChanges();
 
@@ -82,5 +88,32 @@ describe('IiifToolbarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get canvasId from metadata', () => {
+    component.bitstream = Object.assign(new Bitstream(), mockBitstream, {
+      metadata: {
+        'bitstream.iiif.canvasid': [
+          Object.assign(new MetadataValue(), {
+            language: null,
+            value: 'testCanvasId',
+            authority: 'someAuthority',
+          }),
+        ],
+      }
+    });
+    const expectedQueryParams = {
+      canvasId: 'testCanvasId',
+    };
+    component.ngOnInit();
+    expect(component.queryParams).toEqual(expectedQueryParams);
+  });
+
+  it('should get canvasId from uuid', () => {
+    const expectedQueryParams = {
+      canvasId: mockBitstream.uuid,
+    };
+    component.ngOnInit();
+    expect(component.queryParams).toEqual(expectedQueryParams);
   });
 });
