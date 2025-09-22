c028e31997738824d9b3efddade71ec07e39a558
Andrea Barbasso <andrea.barbasso@4science.com>
2025-09-22 14:39:46 +0200

    [UXP-297] fix e2e tests

--- cypress/e2e/admin-workflow-page.cy.ts
+++ cypress/e2e/admin-workflow-page.cy.ts
@@ -16,6 +16,9 @@ describe('Admin Workflow Page', () => {
     // (As we want to scan filter section for accessibility issues as well)
     cy.get('[data-test="filter-toggle"]').click({ multiple: true });
     // Analyze <ds-admin-workflow-page> for accessibility issues
-    testA11y('ds-admin-workflow-page');
+    testA11y({
+      include: ['ds-admin-workflow-page'],
+      exclude: ['.noUi-handle'],
+    });
   });
 });
