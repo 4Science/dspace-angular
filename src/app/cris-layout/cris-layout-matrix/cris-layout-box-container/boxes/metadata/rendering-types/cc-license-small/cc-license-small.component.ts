3365fbe5b0ba9d6a4551ac0b83f5b9ea11bf64ad
Mattia Vianelli <Mattia.Vianelli@4Science.com>
2025-11-10 14:18:42 +0100

    DSC-1817 Made cclicense small component non-standalone

--- src/app/cris-layout/cris-layout-matrix/cris-layout-box-container/boxes/metadata/rendering-types/cc-license-small/cc-license-small.component.ts
+++ src/app/cris-layout/cris-layout-matrix/cris-layout-box-container/boxes/metadata/rendering-types/cc-license-small/cc-license-small.component.ts
@@ -3,25 +3,16 @@ import {
   ItemPageCcLicenseFieldComponent
 } from "../../../../../../../item-page/simple/field-components/specific-field/cc-license/item-page-cc-license-field.component";
 import {RenderingTypeValueModelComponent} from "../rendering-type-value.model";
+import {FieldRenderingType, MetadataBoxFieldRendering} from "../metadata-box.decorator";
 
 @Component({
   selector: 'ds-cc-license-small',
-  standalone: true,
-  imports: [ItemPageCcLicenseFieldComponent],
   templateUrl: './cc-license-small.component.html',
-  styleUrl: './cc-license-small.component.scss'
+  styleUrls: ['./cc-license-small.component.scss']
 })
+@MetadataBoxFieldRendering(FieldRenderingType.CCLICENSESMALL)
 export class CcLicenseSmallComponent extends RenderingTypeValueModelComponent {
-  /**
-   * 'full' variant shows image, a disclaimer (optional) and name (always), better for the item page content.
-   * 'small' variant shows image and name (optional), better for the item page sidebar
-   */
-  @Input() variant = 'small';
 
-  /**
-   * Shows the CC license name with the image. Always show if image fails to load
-   */
-  @Input() showName? = true;
 
   /**
    * Filed name containing the CC license name, as configured in the back-end, in the 'dspace.cfg' file, propertie
