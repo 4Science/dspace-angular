f4f9fb20ebc06163f64b7ceaf73a43c0b00e8f54
Mattia Vianelli <Mattia.Vianelli@4Science.com>
2025-11-10 14:18:34 +0100

    DSC-1817 Made cclicense large component non-standalone

--- src/app/cris-layout/cris-layout-matrix/cris-layout-box-container/boxes/metadata/rendering-types/cc-license-large/cc-license-large.component.ts
+++ src/app/cris-layout/cris-layout-matrix/cris-layout-box-container/boxes/metadata/rendering-types/cc-license-large/cc-license-large.component.ts
@@ -1,27 +1,14 @@
 import { Component, Input } from '@angular/core';
-import {
-  ItemPageCcLicenseFieldComponent
-} from "../../../../../../../item-page/simple/field-components/specific-field/cc-license/item-page-cc-license-field.component";
 import {RenderingTypeValueModelComponent} from "../rendering-type-value.model";
+import {FieldRenderingType, MetadataBoxFieldRendering} from "../metadata-box.decorator";
 
 @Component({
   selector: 'ds-cc-license-large',
-  standalone: true,
-  imports: [ItemPageCcLicenseFieldComponent],
   templateUrl: './cc-license-large.component.html',
-  styleUrl: './cc-license-large.component.scss'
+  styleUrls: ['./cc-license-large.component.scss']
 })
+@MetadataBoxFieldRendering(FieldRenderingType.CCLICENSEFULL)
 export class CcLicenseLargeComponent extends RenderingTypeValueModelComponent {
-  /**
-   * 'full' variant shows image, a disclaimer (optional) and name (always), better for the item page content.
-   * 'small' variant shows image and name (optional), better for the item page sidebar
-   */
-  @Input() variant = 'full';
-
-  /**
-   * Shows the CC license name with the image. Always show if image fails to load
-   */
-  @Input() showName? = true;
 
   /**
    * Filed name containing the CC license name, as configured in the back-end, in the 'dspace.cfg' file, propertie
