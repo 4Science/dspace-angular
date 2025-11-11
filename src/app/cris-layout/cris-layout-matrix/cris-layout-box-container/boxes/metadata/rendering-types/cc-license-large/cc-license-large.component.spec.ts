76f89819b70569babc678783766f60f424224f19
Mattia Vianelli <Mattia.Vianelli@4Science.com>
2025-11-11 14:19:47 +0100

    DSC-1817 Test fix (3)

--- src/app/cris-layout/cris-layout-matrix/cris-layout-box-container/boxes/metadata/rendering-types/cc-license-large/cc-license-large.component.spec.ts
+++ src/app/cris-layout/cris-layout-matrix/cris-layout-box-container/boxes/metadata/rendering-types/cc-license-large/cc-license-large.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TranslateModule } from '@ngx-translate/core';
 import { CcLicenseLargeComponent } from './cc-license-large.component';
 
-fdescribe('CcLicenseLargeComponent', () => {
+describe('CcLicenseLargeComponent', () => {
   let component: CcLicenseLargeComponent;
   let fixture: ComponentFixture<CcLicenseLargeComponent>;
 
