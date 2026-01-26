ebb6262c0fd7d8484d84a762d3434e076dfedf2e
Mattia Vianelli <Mattia.Vianelli@4Science.com>
2026-01-26 15:28:37 +0100

    DSC-2715 Created new component for the submission legend
    (cherry picked from commit f57ec9d46b7ba7675300be1ae68536e736ea8f52)

---
+++ src/app/submission/submission-legend/submission-legend.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ds-submission-legend',
+  templateUrl: './submission-legend.component.html',
+  styleUrls: ['./submission-legend.component.scss']
+})
+export class SubmissionLegendComponent {
+  legendText = 'submission.sections.general.legend';
+}
