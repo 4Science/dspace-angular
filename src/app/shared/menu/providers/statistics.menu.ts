f6d1b826dd636b1044ae5d1dabf8a7e3d9898d1a
Andrea Barbasso <andrea.barbasso@4science.com>
2026-02-04 11:40:18 +0100

    [CST-2709] fix statistics menu

--- src/app/shared/menu/providers/statistics.menu.ts
+++ src/app/shared/menu/providers/statistics.menu.ts
@@ -21,6 +21,8 @@ import { hasValue } from '../../empty.util';
 import { MenuItemType } from '../menu-item-type.model';
 import { PartialMenuSection } from '../menu-provider.model';
 import { DSpaceObjectPageMenuProvider } from './helper-providers/dso.menu';
+import { switchMap, take } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
 
 /**
  * Menu provider to create the "Statistics" menu section in the public navbar. The menu depends on the page it is on.
@@ -29,14 +31,148 @@ import { DSpaceObjectPageMenuProvider } from './helper-providers/dso.menu';
  */
 @Injectable()
 export class StatisticsMenuProvider extends DSpaceObjectPageMenuProvider {
+  private activatedRouteLastChild: any;
 
   constructor(
     protected authorizationService: AuthorizationDataService,
+    protected route: ActivatedRoute
   ) {
     super();
   }
 
+  /**
+   *  Get statistics route dso data
+   */
+  getObjectUrl(data) {
+    const object = data.site ? data.site : data.dso?.payload;
+    return object?._links?.self?.href;
+  }
+
+  /**
+   *  Get activated route of the deepest activated route
+   */
+  getActivatedRoute(route) {
+    if (route.children.length > 0) {
+      return this.getActivatedRoute(route.firstChild);
+    } else {
+      return route;
+    }
+  }
+
+  /**
+   *  Checking authorization for Usage
+   */
+  getAuthorizedUsageStatistics() {
+    return this.activatedRouteLastChild.data.pipe(
+      switchMap((data) => {
+        return this.authorizationService.isAuthorized(FeatureID.CanViewUsageStatistics, this.getObjectUrl(data)).pipe(
+          map((canViewUsageStatistics: boolean) => {
+            return canViewUsageStatistics;
+          }));
+      }),
+    );
+  }
+
+  /**
+   *  Checking authorization for Login
+   */
+  getAuthorizedLoginStatistics() {
+    return this.activatedRouteLastChild.data.pipe(
+      switchMap((data) => {
+        return this.authorizationService.isAuthorized(FeatureID.CanViewLoginStatistics, this.getObjectUrl(data)).pipe(
+          map((canViewLoginStatistics: boolean) => {
+            return canViewLoginStatistics;
+          }));
+      }),
+    );
+  }
+
+  /**
+   *  Checking authorization for Workflow
+   */
+  getAuthorizedWorkflowStatistics() {
+    return this.activatedRouteLastChild.data.pipe(
+      switchMap((data) => {
+        return this.authorizationService.isAuthorized(FeatureID.CanViewWorkflowStatistics, this.getObjectUrl(data)).pipe(
+          map((canViewWorkflowStatistics: boolean) => {
+            return canViewWorkflowStatistics;
+          }));
+      }),
+    );
+  }
+
   public getSectionsForContext(dso: DSpaceObject): Observable<PartialMenuSection[]> {
+    this.activatedRouteLastChild = this.getActivatedRoute(this.route);
+    return combineLatest({
+      canViewUsage: this.getAuthorizedUsageStatistics(),
+      canViewLogin: this.getAuthorizedLoginStatistics(),
+      canViewWorkflow: this.getAuthorizedWorkflowStatistics(),
+    }).pipe(
+      take(1),
+      map(({canViewUsage, canViewLogin, canViewWorkflow}) => {
+      const menuList = [];
+      if (canViewUsage || canViewLogin || canViewWorkflow) {
+        if (canViewUsage) {
+          menuList.push({
+            id: 'statistics_site',
+            parentID: 'statistics',
+            active: false,
+            visible: true,
+            model: {
+              type: MenuItemType.LINK,
+              text: 'menu.section.statistics.site',
+              link: '/statistics',
+            },
+          });
+        }
+
+        if (canViewLogin) {
+          menuList.push({
+            id: 'statistics_login',
+            parentID: 'statistics',
+            active: false,
+            visible: true,
+            model: {
+              type: MenuItemType.LINK,
+              text: 'menu.section.statistics.login',
+              link: '/statistics/login',
+            },
+          });
+        }
+
+        if (canViewWorkflow) {
+          menuList.push({
+            id: 'statistics_workflow',
+            parentID: 'statistics',
+            active: false,
+            visible: true,
+            model: {
+              type: MenuItemType.LINK,
+              text: 'menu.section.statistics.workflow',
+              link: '/statistics/workflow',
+            },
+          });
+        }
+
+        // the parent menu should be added after the children
+        menuList.push(
+          {
+            id: 'statistics',
+            active: false,
+            visible: true,
+            index: 1,
+            model: {
+              type: MenuItemType.TEXT,
+              text: 'menu.section.statistics',
+            },
+          },
+        );
+      }
+      return menuList;
+    }));
+  }
+
+  public getSectionsForContext2(dso: DSpaceObject): Observable<PartialMenuSection[]> {
     return combineLatest([
       this.authorizationService.isAuthorized(FeatureID.CanViewUsageStatistics, dso?._links.self.href),
     ]).pipe(
