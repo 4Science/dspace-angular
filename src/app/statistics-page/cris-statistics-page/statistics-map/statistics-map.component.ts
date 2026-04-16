c2edb8bff6bcb1953e66cf1af4b2e652ca0c412f
FrancescoMolinaro <francesco.molinaro@4science.com>
2026-04-16 12:04:55 +0200

    [DSC-2811] fix issue with image exports, add tests

--- src/app/statistics-page/cris-statistics-page/statistics-map/statistics-map.component.ts
+++ src/app/statistics-page/cris-statistics-page/statistics-map/statistics-map.component.ts
@@ -1,5 +1,6 @@
 import {
   AsyncPipe,
+  DOCUMENT,
   isPlatformBrowser,
 } from '@angular/common';
 import {
@@ -28,6 +29,10 @@ import {
   ExportImageType,
   ExportService,
 } from '../../../core/export-service/export.service';
+import {
+  NativeWindowRef,
+  NativeWindowService,
+} from '../../../core/services/window.service';
 import {
   getFirstSucceededRemoteData,
   getRemoteDataPayload,
@@ -93,10 +98,12 @@ export class StatisticsMapComponent implements OnInit {
   constructor(
     @Inject(PLATFORM_ID) protected platformId: any,
     private usageReportService: UsageReportDataService,
+    @Inject(NativeWindowService) private _window: NativeWindowRef,
+    @Inject(DOCUMENT) private document: any,
   ) {
     if (isPlatformBrowser(this.platformId)) {
       import('../../../core/export-service/browser-export.service').then((s) => {
-        this.exportService = new s.BrowserExportService(this.platformId);
+        this.exportService = new s.BrowserExportService(this.platformId, this._window, this.document);
       });
     } else {
       import('../../../core/export-service/server-export.service').then((s) => {
