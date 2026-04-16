33c212f34cd0c0b0e1149467f6802e845467ad55
FrancescoMolinaro <francesco.molinaro@4science.com>
2026-04-16 17:39:52 +0200

    [DSC-2599] fix error clearing on blur

--- src/app/shared/form/form.service.spec.ts
+++ src/app/shared/form/form.service.spec.ts
@@ -278,7 +278,7 @@ describe('FormService test suite', () => {
 
     service.removeErrorFromField(control, model, 'error.required');
 
-    expect(control.errors).toEqual({ required: null });
+    expect(control.errors).toEqual(null);
   });
 
   it('should remove errors from fields of concat group', () => {
@@ -300,7 +300,7 @@ describe('FormService test suite', () => {
 
     // the group's inputs should no longer have an error
     Object.values(control.controls).forEach((subControl: AbstractControl) => {
-      expect(control.errors).toEqual({ [messageKey]: null });
+      expect(control.errors).toEqual(null);
     });
   });
 
