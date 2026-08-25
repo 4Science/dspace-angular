fabc596ebd1e5dbee8dd103e4504b490c3913fd8
FrancescoMauto <francesco.mauto@4science.com>
2026-08-25 15:52:06 +0200

    [DSC-2308] add: skeleton loader on browse elements sub components

--- src/app/shared/browse-most-elements/default-browse-elements/default-browse-elements.component.ts
+++ src/app/shared/browse-most-elements/default-browse-elements/default-browse-elements.component.ts
@@ -1,19 +1,34 @@
-import { Component, OnChanges, OnInit } from '@angular/core';
+import {
+  Component,
+  OnChanges,
+  OnInit,
+} from '@angular/core';
+
 import { AbstractBrowseElementsComponent } from '../abstract-browse-elements.component';
 
 @Component({
   selector: 'ds-default-browse-elements',
   templateUrl: './default-browse-elements.component.html',
-  styleUrls: ['./default-browse-elements.component.scss']
+  styleUrls: ['./default-browse-elements.component.scss'],
 })
 export class DefaultBrowseElementsComponent extends AbstractBrowseElementsComponent implements OnInit, OnChanges {
 
   protected followMetricsLink: boolean;
   protected followThumbnailLink: boolean;
 
+  /**
+   * Array used to render skeleton placeholder items.
+   * Length matches the configured page size.
+   */
+  skeletonItems: number[];
+
   ngOnInit() {
     this.followMetricsLink = this.showMetrics ?? this.appConfig.browseBy.showMetrics;
     this.followThumbnailLink = this.showThumbnails ?? this.appConfig.browseBy.showThumbnails;
+    this.skeletonItems = Array.from(
+      { length: this.paginatedSearchOptions?.pagination?.pageSize ?? 5 },
+      (_, i) => i,
+    );
     super.ngOnInit();
   }
 }
