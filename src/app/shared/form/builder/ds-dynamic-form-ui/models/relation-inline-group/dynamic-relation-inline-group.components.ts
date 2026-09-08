6e9fc15f25ec8b6f1a222072d4408df0d120268b
Mykhaylo Boychuk <mykhaylo.boychuk@4science.com>
2026-09-08 15:57:23 +0200

    [DSC-2010] Highlight only the invalid row of an inline-group

--- src/app/shared/form/builder/ds-dynamic-form-ui/models/relation-inline-group/dynamic-relation-inline-group.components.ts
+++ src/app/shared/form/builder/ds-dynamic-form-ui/models/relation-inline-group/dynamic-relation-inline-group.components.ts
@@ -109,6 +109,7 @@ export class DsDynamicRelationInlineGroupComponent extends DynamicFormControlCom
       initialCount: isNotEmpty(this.model.value) ? (this.model.value as any[]).length : 1,
       isDraggable: true,
       isInlineGroupArray: true,
+      mandatoryField: this.model.mandatoryField,
       groupFactory: () => {
         const fieldValue = isEmpty(this.model.value) || (arrayCounter === 0) ? {} : this.model.value[arrayCounter - 1];
         const model: any = this.initArrayItemModel(formConfig, this.normalizeGroupFormValue(fieldValue));
